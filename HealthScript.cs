d0e11c603a6d3f6b4d079a10e832e905238d0487
Rishabh Bhatt <rishabhbhatt159@gmail.com>
2020-08-31 21:34:39 +0530

    Initital 6 files added

---
+++ HealthScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthScript : MonoBehaviour {
+
+	//public GameObject duplication;
+	private float Health=100.0f;
+	//private Transform mytomb;
+	// Use this for initialization
+	void Start () {
+		//mytomb = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject != null) {
+			if (Health <= 0) {
+				DeathMethod ();
+				Destroy (this.gameObject);
+			}
+
+		} 
+	}
+	void OnCollisionExit(Collision other)
+	{
+		if (other.gameObject.CompareTag("Duplicate")) {
+			Health -= 1f;
+			Debug.Log(Health);
+		
+			//GameObject.Instantiate (duplication, mytomb);
+		}
+	}
+	void DeathMethod()
+	{
+		SceneManager.LoadScene ("scenesimple");
+	}
+}
